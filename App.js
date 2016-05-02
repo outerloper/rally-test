a790940ee1a19a251708255d63ab6bbca37064a8
outerloper <Przytul4>
2016-05-02 16:24:24 +0200

    Grunt update

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 Ext.define('CustomApp', {
-   extend: 'Rally.app.App',
-   componentCls: 'app',
-   launch: function()
-   {
-      alert("Hello from App");
-      console.log("App");
-   }
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    launch: function () {
+        alert("Hello from App");
+        console.log("App");
+    }
 });
--- README.md
+++ README.md
@@ -4,10 +4,12 @@ RallyTest
 ## Overview
 
 
+## Dev notes
+
 ## License
 
 RallyTest is released under the MIT license.  See the file [LICENSE](./LICENSE) for the full text.
 
-##Documentation for SDK
+## Documentation for SDK
 
 You can find the documentation on our help [site.](https://help.rallydev.com/apps/2.1/doc/)
